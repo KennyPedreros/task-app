be45003b2b7274f9375243023ace0e8590766f90
KennyPedreros <kennypedreros10@gmail.com>
2026-03-04 17:16:08 -0500

    add resource controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TaskController;
+
+Route::resource('categories', CategoryController::class);
+Route::resource('tasks', TaskController::class);
 
 Route::get('/', function () {
     return view('welcome');
